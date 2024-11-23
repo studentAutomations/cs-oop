0dfbe9b6cd65399c1fe6ae94b64a67fb067142e6
studentAutomations <student.automations@gmail.com>
2024-11-23 18:10:00 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -15,10 +15,9 @@ hook.send('@everyone Nova obavestenja na CS-u!')
 image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
 image2 = 'https://github.com/studentAutomations/cs-oop/blob/main/cs-oop-nova-obavestenja.png'
 
-embed.set_author(name='Elektronski Fakultet')
 embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
 embed.add_field(name='Vidi obavestenja - ', value=image2)
-embed.set_footer(text='by og._.njen')
+embed.set_footer(text='Elektronski Fakultet')
 embed.set_thumbnail(image1)
 
 hook.send(embed=embed)
