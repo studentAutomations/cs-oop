eeb740db480b55cddb5c6595df09e4b69ac010a0
studentAutomations <student.automations@gmail.com>
2024-11-24 16:37:59 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -2,22 +2,26 @@ import os
 from dhooks import Webhook, Embed
 
 # Get the webhook URL from environment variable
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')
-hook = Webhook(WEBHOOK_URL) 
+WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 
-embed = Embed(
-    color= 0x499957,
-    timestamp='now'
-)
 
-hook.send('@everyone Nova obavestenja na CS-u!')
+for url in WEBHOOK_URL:
+    hook = Webhook(url) 
 
-image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
-image2 = 'https://github.com/studentAutomations/cs-oop/blob/main/cs-oop-nova-obavestenja.png'
+    embed = Embed(
+        color= 0x499957,
+        timestamp='now'
+    )
 
-embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
-embed.add_field(name='Vidi obavestenja - ', value=image2)
-embed.set_footer(text='Elektronski Fakultet')
-embed.set_thumbnail(image1)
+    hook.send('@everyone Nova obavestenja na CS-u!')
 
-hook.send(embed=embed)
+    image1 = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQOmPtpzziqcXbG795QYLmYvJl6G6CcyKbKHA&s'
+    image2 = 'https://github.com/studentAutomations/cs-oop/blob/main/cs-oop-nova-obavestenja.png'
+
+
+    embed.add_field(name='Idi na sajt - ', value='https://cs.elfak.ni.ac.rs/nastava/')
+    embed.add_field(name='Vidi obavestenja - ', value=image2)
+    embed.set_footer(text='Elektronski Fakultet')
+    embed.set_thumbnail(image1)
+
+    hook.send(embed=embed)
