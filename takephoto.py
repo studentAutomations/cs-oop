3b4e415401a1719f3bf82f9fa9471775f843c05d
studentAutomations <student.automations@gmail.com>
2024-11-25 23:33:14 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -5,21 +5,21 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-# Set up Chrome options for headless operation
+
 chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run Chrome in headless mode
+chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = "/usr/bin/chromium-browser"  # Set the path to the Chromium binary
+chrome_options.binary_location = "/usr/bin/chromium-browser"  
 
-# Path to the ChromeDriver executable
+
 browser_driver = Service('/usr/bin/chromedriver')
 
-# Initialize the WebDriver with options
+
 page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
 
 try:
-    # Navigate to the page and perform login actions
+    
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/")
     page_to_scrape.find_element(By.LINK_TEXT, "Log in").click()
     time.sleep(5)
@@ -28,12 +28,12 @@ try:
     time.sleep(5)
 
     mail = page_to_scrape.find_element(By.ID, "i0116")
-    mail.send_keys(os.environ['MAIL'])  # Use environment variable for email
+    mail.send_keys(os.environ['MAIL'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
     time.sleep(5)
 
     password = page_to_scrape.find_element(By.ID, "i0118")
-    password.send_keys(os.environ['PASSWORD'])  # Use environment variable for password
+    password.send_keys(os.environ['PASSWORD'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
     time.sleep(5)
 
@@ -50,24 +50,23 @@ try:
 
     responseT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
-             # Get the size of the element
+             
     height = responseT.size['height']
     width = responseT.size['width']
 
-     # Set a larger window size (you can adjust these values as needed)
-    # Here we set a fixed width that is larger than what might be needed.
-    desired_width = max(width, 1200)  # Ensure at least 1200px width
+   
+    desired_width = max(width, 1200)  
 
     desired_height = min(height, 1000)
 
-    page_to_scrape.set_window_size(desired_width, desired_height)  # Adding some extra space
+    page_to_scrape.set_window_size(desired_width, desired_height) 
 
-    # Scroll to make sure the element is visible in case it's off-screen
+    
     page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
 
-    # Take a screenshot of the entire element
+    
     responseT.screenshot('cs-oop-nova-obavestenja.png')
 
 finally:
-    # Clean up by quitting the driver
+    
     page_to_scrape.quit()
