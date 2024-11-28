6ab1c3d09d3a5a25d75de5c583cf3950f4a3f9e2
studentAutomations <student.automations@gmail.com>
2024-11-28 03:13:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,59 +5,50 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
-
 chrome_options = Options()
 chrome_options.add_argument("--headless")  
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = "/usr/bin/chromium-browser" 
+chrome_options.binary_location = "/usr/bin/google-chrome"  # Updated for GitHub runner
 
-
-browser_driver = Service('/usr/bin/chromedriver')
-
+browser_driver = Service('/usr/bin/chromedriver')  # Updated for GitHub runner
 
 page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
 
 try:
-    
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/")
     page_to_scrape.find_element(By.LINK_TEXT, "Log in").click()
-    time.sleep(5)
+    time.sleep(2)
 
     page_to_scrape.find_element(By.LINK_TEXT, "OpenID Connect").click()
-    time.sleep(5)
+    time.sleep(2)
 
     mail = page_to_scrape.find_element(By.ID, "i0116")
     mail.send_keys(os.environ['MAIL'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
-    time.sleep(5)
+    time.sleep(2)
 
     password = page_to_scrape.find_element(By.ID, "i0118")
     password.send_keys(os.environ['PASSWORD'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
-    time.sleep(5)
+    time.sleep(2)
 
     page_to_scrape.find_element(By.ID, "idBtn_Back").click()
-    time.sleep(5)
+    time.sleep(2)
 
-    page_to_scrape.find_element(By.LINK_TEXT, "OOP").click()
-    time.sleep(5)
-    
+    page_to_scrape.find_element(By.LINK_TEXT, "LP").click()
+    time.sleep(2)
+
     link_element = page_to_scrape.find_element(By.XPATH, "/html/body/div[1]/div[2]/div/div[1]/section/div/div/ul/li[1]/div[3]/ul/li/div/div/div[2]/div/a/span")
     link_element.click()
-    time.sleep(5)
-
+    time.sleep(2)
 
     responseT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
 
-   
-    
     novosti_markdown = responseT.text  
 
-    
     with open("novosti.md", "w") as novosti_file:
         novosti_file.write(novosti_markdown)
 
 finally:
-   
     page_to_scrape.quit()
