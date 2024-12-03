03700ed6ec5b6700c456dea39c200bb565ec6669
studentAutomations <student.automations@gmail.com>
2024-12-03 15:32:58 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,7 +1,7 @@
 import os
 from dhooks import Webhook, Embed, File
 
-WEBHOOK_URL = [os.getenv('PB')]
+WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 for url in WEBHOOK_URL:
     hook = Webhook(url) 
 
