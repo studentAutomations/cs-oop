125d55d258095342a0ca4950e621cb7e805a58f1
studentAutomations <student.automations@gmail.com>
2024-12-03 20:20:08 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -1,14 +1,16 @@
 import os
 from dhooks import Webhook, Embed, File
 
+image2_path = 'cs-oop-nova-obavestenja.png'
+
 WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN'), os.getenv('WEBHOOK_OTHER1')]
 for url in WEBHOOK_URL:
-    hook = Webhook(url) 
+    hook = Webhook(url)
 
-    hook.send('**@everyone**')
-    
-    image2_path = 'cs-oop-nova-obavestenja.png'  
+    embed = Embed(
+        description="**@everyone**\n\n>>> [**CS link**](https://cs.elfak.ni.ac.rs/nastava/)",
+        color=0x3498DB
+    )
+    embed.set_image(url=f"attachment://{image2_path}")  
 
-    hook.send(file=File(image2_path, name='cs-oop-nova-obavestenja.png'))
-
-    hook.send('**>>> https://cs.elfak.ni.ac.rs/nastava/**')
+    hook.send(file=File(image2_path, name='cs-oop-nova-obavestenja.png'), embed=embed)
