76f24fe1a2698b6e6fd94e433439bbeaaed257e4
studentAutomations <student.automations@gmail.com>
2024-12-05 00:30:42 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -8,9 +8,10 @@ for url in WEBHOOK_URL:
     hook = Webhook(url)
 
     embed = Embed(
-        description="**@everyone**\n\n>>> **[CS link](https://cs.elfak.ni.ac.rs/nastava/)**",
+        description="**[CS link](https://cs.elfak.ni.ac.rs/nastava/)**",
         color=0x3498DB
     )
-    embed.set_image(url=f"attachment://{image2_path}")  
 
-    hook.send(file=File(image2_path, name='cs-oop-nova-obavestenja.png'), embed=embed)
+    embed.set_image(url="attachment://cs-oop-nova-obavestenja.png")
+    file = File(image2_path, name="cs-oop-nova-obavestenja.png")
+    hook.send("**@everyone 📢 OOP**", embed=embed, file=file)
