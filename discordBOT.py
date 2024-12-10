ca2ea1624596db97a9ab01aeb8124e4def3ab387
studentAutomations <student.automations@gmail.com>
2024-12-10 10:34:01 +0100

    Update discordBOT.py

--- discordBOT.py
+++ discordBOT.py
@@ -3,12 +3,12 @@ from dhooks import Webhook, Embed, File
 
 image2_path = 'cs-oop-nova-obavestenja.png'
 
-WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN')]
+WEBHOOK_URL = [os.getenv('PB')]
 for url in WEBHOOK_URL:
     hook = Webhook(url)
 
     embed = Embed(
-        description="**[CS link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/view.php?id=1051)**",
+        description="**[CS link - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
         color=0x3498DB
     )
 
