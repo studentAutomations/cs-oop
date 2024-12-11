03cfd99cd8593c28a9c890601c0ed93781ba6cad
studentAutomations <student.automations@gmail.com>
2024-12-11 18:02:32 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -47,12 +47,20 @@ try:
     # Step 6: Navigate to the forum search page
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
 
-    # Step 7: Extract data and save it to a markdown file
-    responseT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
-    novosti_markdown = responseT.text
+    height = responseT.size['height']
+    width = responseT.size['width']
 
-    with open("novosti.md", "w") as novosti_file:
-        novosti_file.write(novosti_markdown)
+    
+    desired_width = max(width, 1200)  
+
+    desired_height = min(height, 1000)
+
+    page_to_scrape.set_window_size(desired_width, desired_height)  
+
+   
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
+
+    responseT.screenshot('cs-oop-nova-obavestenja.png')
 
 finally:
     # Close the browser
