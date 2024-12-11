9e43ba2a629242193aaafc159127177a51cd89fd
studentAutomations <student.automations@gmail.com>
2024-12-11 17:59:27 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -5,57 +5,55 @@ from selenium.webdriver.chrome.options import Options
 import time
 import os
 
+# Chrome options
 chrome_options = Options()
-chrome_options.add_argument("--headless")  
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.binary_location = "/usr/bin/google-chrome"  # Updated for GitHub runner
+chrome_options.binary_location = "/usr/bin/google-chrome"
 
-browser_driver = Service('/usr/bin/chromedriver')  # Updated for GitHub runner
+# Chrome driver setup
+browser_driver = Service('/usr/bin/chromedriver')
 
+# Start the browser
 page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
 
 try:
+    # Step 1: Navigate to the page
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/")
     page_to_scrape.find_element(By.LINK_TEXT, "Log in").click()
     time.sleep(2)
 
+    # Step 2: Log in via OpenID Connect
     page_to_scrape.find_element(By.LINK_TEXT, "OpenID Connect").click()
     time.sleep(2)
 
+    # Step 3: Input email
     mail = page_to_scrape.find_element(By.ID, "i0116")
     mail.send_keys(os.environ['MAIL'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
     time.sleep(2)
 
+    # Step 4: Input password
     password = page_to_scrape.find_element(By.ID, "i0118")
     password.send_keys(os.environ['PASSWORD'])  
     page_to_scrape.find_element(By.ID, "idSIButton9").click()
     time.sleep(2)
 
+    # Step 5: Click "No" on "Stay signed in" prompt
     page_to_scrape.find_element(By.ID, "idBtn_Back").click()
     time.sleep(2)
 
+    # Step 6: Navigate to the forum search page
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
 
-
+    # Step 7: Extract data and save it to a markdown file
     responseT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    novosti_markdown = responseT.text
 
-    height = responseT.size['height']
-    width = responseT.size['width']
-
-   
-    desired_width = max(width, 1200)  
-
-    desired_height = min(height, 1000)
-
-    page_to_scrape.set_window_size(desired_width, desired_height) 
-
-    
-    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseT)
-
-    
-    responseT.screenshot('cs-oop-nova-obavestenja.png')
+    with open("novosti.md", "w") as novosti_file:
+        novosti_file.write(novosti_markdown)
 
 finally:
+    # Close the browser
     page_to_scrape.quit()
