daba62e92854467f9fe6e9d5d7c4c9d3de2e5ac1
studentAutomations <student.automations@gmail.com>
2024-12-12 11:39:22 +0100

    Update takephoto.py

--- takephoto.py
+++ takephoto.py
@@ -47,6 +47,8 @@ try:
     # Step 6: Navigate to the forum search page
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2023&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
 
+    responseT = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    
     height = responseT.size['height']
     width = responseT.size['width']
 
